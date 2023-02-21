614d053417f92d6c7951578035b52c1d69f4861d
vincent <vincent.delacour@iepg.fr>
2023-02-21 10:08:57 +0100

    update

--- src/Controller/CasLogout.php
+++ src/Controller/CasLogout.php
@@ -7,23 +7,16 @@ class CasLogout
     private String $cas_host;
     private String $cas_path;
     private Int $cas_port;
-    private Bool $cas_ca;
-    private String $cas_ca_path;
 
     public function __construct(
         String $cas_host,
         String $cas_path,
-        Int $cas_port,
-        Bool $cas_ca,
-        String $cas_ca_path
+        Int $cas_port
     ) {
         $this->cas_host = $cas_host;
         $this->cas_path = $cas_path;
         $this->cas_port = $cas_port;
-        $this->cas_ca = $cas_ca;
-        $this->cas_ca_path = $cas_ca_path;
     }
-
 
     public function logout($request): void
     {
@@ -32,13 +25,14 @@ class CasLogout
             $request->getSchemeAndHttpHost()
             :
             $this->cas_path;
-        \phpCAS::client(
+            \phpCAS::client(
             CAS_VERSION_2_0,
             $this->cas_host,
             $this->cas_port,
             '',
             $cas_path
         );
+
         \phpCAS::handleLogoutRequests();
         \phpCAS::logout();
         \phpCAS::forceAuthentication();
