2fed17569ac5ec66691a6e3c4f277e31169850ba
dsi-iepg <dsi-iepg@iepg.fr>
2025-12-12 10:59:17 +0100

    patch defult dispatch views

--- src/Controller/CasController.php
+++ src/Controller/CasController.php
@@ -8,59 +8,36 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class CasController extends AbstractController
 {
-    #[Route('/cas_anonymous-home', name:'cas_anonymous_home')]
-    public function index(): Response
-    {
-        return $this->render('@cas_connection/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
-
-    #[Route('/cas_user-home', name:'cas_user_home')]
-    public function indexUser(): Response
-    {
-        $user = $this->getUser();
-
-        return $this->render('@cas_connection/index_user.html.twig', [
-            'controller_name' => 'HomeController',
-            'user' => $user,
-        ]);
-    }
-
-    #[Route('/cas_admin-home', name:'cas_admin_home')]
-    public function indexAdmin(): Response
-    {
-        $admin = $this->getUser();
-
-        return $this->render('@cas_connection/index_admin.html.twig', [
-            'controller_name' => 'HomeController',
-            'admin' => $admin,
-        ]);
-    }
-
-    #[Route('/cas_unknow-home', name:'cas_user_unknow')]
-    public function indexUserUnknow(): Response
-    {
-        return $this->render('@cas_connection/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
-
-    #[Route('/cas_login', name:'cas_login')]
+    #[Route('/cas_login', name: 'cas_login')]
     public function cas_authenticator(): Response
     {
         return $this->redirectToRoute('cas_dispatcher');
     }
 
-    #[Route('/cas_dispatcher', name:'cas_dispatcher', methods:'GET')]
+    #[Route('/cas_dispatcher', name: 'cas_dispatcher', methods: 'GET')]
     public function dispatcher()
     {
         if ($this->isGranted('ROLE_ADMIN')) {
-            return $this->redirectToRoute('cas_admin_home');
+
+            $admin = $this->getUser();
+
+            return $this->render('@cas_connection/index_admin.html.twig', [
+                'controller_name' => 'HomeController',
+                'admin' => $admin,
+            ]);
         };
         if ($this->isGranted('ROLE_USER')) {
-            return $this->redirectToRoute('cas_user_home');
+
+            $user = $this->getUser();
+
+            return $this->render('@cas_connection/index_user.html.twig', [
+                'controller_name' => 'HomeController',
+                'user' => $user,
+            ]);
         };
-        return $this->redirectToRoute('cas_anonymous_home');
+
+        return $this->render('@cas_connection/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
     }
 }
